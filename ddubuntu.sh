8a01ef7bc8a19077f984f61636c842ae29a243aa
mzcoo <89264156+mzcoo@users.noreply.github.com>
2021-10-26 11:20:49 -0400

    Update ddubuntu.sh

--- ddubuntu.sh
+++ ddubuntu.sh
@@ -5,4 +5,4 @@ export SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/"
 export value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
 export ipM="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
 wget https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh
-bash ./InstallNET.sh -u 20.04 -p Vm54w8GcUc7Hp1c8EP471halYuRUKuso5Ti9Ds0dUzDhEVdqCmjlhiYkUeQuKHIgYTSwgDydnmIXw8iK5Vdh3o00nDxNuFt8uBnPZQjod9VcENHTFNCkVMKTsmrGZjxJ -apt http://archive.ubuntu.com/ubuntu --ip-addr $ipA --ip-mask $ipM --ip-gate $ipG --ip-dns 208.67.220.220 -port 19260 --noipv6 -a
+bash ./InstallNET.sh -u 20.04 -p Vm54w8GcUc7Hp1c8EP471halYxJ -apt http://archive.ubuntu.com/ubuntu --ip-addr $ipA --ip-mask $ipM --ip-gate $ipG --ip-dns 208.67.220.220 -port 19260 --noipv6 -a
